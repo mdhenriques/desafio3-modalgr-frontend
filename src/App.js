18075fc4dd52d85b039d6c91301750725050af2a
mdhenriques <md.henriques@outlook.com>
2023-08-31 14:24:53 -0300

    Criando primeiros campos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import Banner from './components/Banner/Banner.js';
+import Banner from './components/Banner';
+import TextField from './components/TextField';
 
 function App() {
   return (
     <div className="App">
       <Banner />      
+      <TextField label="Username" placeholder="Enter your username..."/>
+      <TextField label="Password" placeholder="Enter your password..."/>
     </div>
   );
 }
